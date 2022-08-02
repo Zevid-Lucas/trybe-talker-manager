bcaceaaa6806095fb17d30a0f93f6d890b37408d
David Costa <zevid.dev@outlook.com>
2022-08-02 17:59:06 -0300

    ✨ feat(req03-04):add post /login com validacoes p/ email, senha

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { readTalkers } = require('./readAndWriteTalker');
+const generateToken = require('./generateToken');
 
 const app = express();
 app.use(bodyParser.json());
@@ -26,6 +27,21 @@ app.get('/talker/:id', async (req, res) => {
   if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(200).json(talkerById);
+});
+
+app.post('/login', (req, res) => {
+  const regexEmail = /\S+@\S+\.\S+/;
+  const { email, password } = req.body;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!regexEmail.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const token = generateToken();
+  return res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
