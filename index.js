a5ebcbfad117abfb38ddfb150153381bdeecb301
David Costa <zevid.dev@outlook.com>
2022-08-02 17:47:22 -0300

    ✨ feat(req02):add get /talker/:id

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ app.get('/talker', async (req, res) => {
   return res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkers();
+  const talkerById = talkers.find((talker) => talker.id === Number(id));
+
+  if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json(talkerById);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
