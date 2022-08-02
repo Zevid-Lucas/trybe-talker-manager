96972010113389d98eb89d05f60fa19152fc9fab
David Costa <zevid.dev@outlook.com>
2022-08-02 19:49:13 -0300

    🐞 fix(req06):Adiciona mais uma verificacao no validateRate

--- validationsTalker.js
+++ validationsTalker.js
@@ -43,10 +43,10 @@ function validateWatchedAt(req, res, next) {
 
 function validateRate(req, res, next) {
   const { rate } = req.body.talk;
-  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  if (rate < 1 || rate > 5) {
+  if (rate < 1 || rate > 5 || rate === '') {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
+  if (!rate) return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
   next();
 }
 
